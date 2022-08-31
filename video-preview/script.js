17cb364b15768acb7953189df0464adf9f9c520e
2anny2 <anny192922@gmail.com>
2022-08-31 15:01:38 -0400

    first commit

---
+++ video-preview/script.js
@@ -0,0 +1,19 @@
+function playvideo(peace){
+    peace.play();
+}
+
+function pausarvideo(peace){
+    peace.pause();
+}
+
+function cambiar_texto(element){
+    element.textContent = "Suscrito";
+}
+
+function addsombra(vid){
+    vid.classList.add("sombra");
+}
+
+function removesombra(vid){
+    vid.classList.remove("sombra");
+}
